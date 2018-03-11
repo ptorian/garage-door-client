92fc1bcc310d48ef6b4eef8c513dbf758cc8f9d7
Sean Price <seanprice@sevenhillstechnology.com>
2018-03-11 10:26:34 -0400

    Open webpack dev server to public, use hardcoded ip addresses instead of localhost for api/socket urls for testing

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,8 @@ module.exports = {
             inject: true
         }),
         new webpack.DefinePlugin({
-            API_URL: JSON.stringify(process.env.API_URL || "http://localhost:8000/api"),
-            SOCKET_IO_URL: JSON.stringify(process.env.SOCKET_IO_URL || "http://localhost:8000")
+            API_URL: JSON.stringify(process.env.API_URL || "http://192.168.0.113:8000/api"),
+            SOCKET_IO_URL: JSON.stringify(process.env.SOCKET_IO_URL || "http://192.168.0.113:8000")
         })
     ]
 };
